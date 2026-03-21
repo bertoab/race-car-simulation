a36bf2f6a04af334a34b80f955b0e4d8cd39f657
saplio <262173844+saplio@users.noreply.github.com>
2026-03-21 18:19:37 -0400

    Test

---
+++ Main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("test");
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
