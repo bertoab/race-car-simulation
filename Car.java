be62dc01f5e87ad55e8dd5ff80e185e5cb342694
Joshua Staub <18756667+Carbonyte@users.noreply.github.com>
2026-03-23 14:52:01 -0400

    Basic Car class, StatusEffect enum

---
+++ Car.java
@@ -0,0 +1,98 @@
+// Joshua Staub
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Car {
+    private String name;
+    private double speed;
+    private int currentTrackIndex;
+    private final int goalTrackIndex;
+    private final int totalTracks;
+    private double positionInTrackSection;
+    private Set<StatusEffect> statusEffects;
+
+    public Car(String name, int startTrackIndex, int goalTrackIndex, int totalTracks) {
+        this.name = name;
+        this.currentTrackIndex = startTrackIndex;
+        this.goalTrackIndex = goalTrackIndex;
+        this.totalTracks = totalTracks;
+        speed = 0;
+        positionInTrackSection = 0;
+        statusEffects = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = Math.max(0.0, speed);
+    }
+
+    public double getPosition() {
+        return positionInTrackSection;
+    }
+
+    public void setPosition(double position) {
+        if (position < 0.0) {
+            return;
+        }
+
+        if (hasFinished()) {
+            this.positionInTrackSection = 0;
+            return;
+        }
+
+        if (position >= 1.0) {
+            // If the position is 1 or greater, then move to the next track
+            int trackIncrement = (int) Math.floor(position);
+
+            // Avoid overshooting the goal track
+            int tracksRemaining = Math.floorMod(goalTrackIndex - currentTrackIndex, totalTracks);
+            if (tracksRemaining < trackIncrement) {
+                this.currentTrackIndex = goalTrackIndex;
+                this.positionInTrackSection = 0.0;
+            } else {
+                this.currentTrackIndex = (currentTrackIndex + trackIncrement) % totalTracks;
+                this.positionInTrackSection = trackIncrement - position;
+            }
+        } else {
+            this.positionInTrackSection = position;
+        }
+    }
+
+    public void setCurrentTrackIndex(int trackIndex) {
+        this.currentTrackIndex = trackIndex % totalTracks;
+        this.positionInTrackSection = 0.0;
+    }
+
+    public int getCurrentTrackIndex() {
+        return currentTrackIndex;
+    }
+
+    public boolean hasFinished() {
+        return goalTrackIndex == currentTrackIndex;
+    }
+
+    public Set<StatusEffect> getStatusEffects() {
+        return Collections.unmodifiableSet(statusEffects);
+    }
+
+    public void addEffect(StatusEffect statusEffect) {
+        statusEffects.add(statusEffect);
+    }
+
+    public void removeEffect(StatusEffect statusEffect) {
+        statusEffects.remove(statusEffect);
+    }
+}
---
+++ StatusEffect.java
@@ -0,0 +1,11 @@
+// Joshua Staub
+
+public enum StatusEffect {
+    MUD,
+    CONCRETE,
+    SNOW,
+    TIRE_POPPED,
+    OFF_TRACK,
+    UPHILL,
+    DOWNHILL
+}
