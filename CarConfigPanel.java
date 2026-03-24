f70cb8930969a35fa7f65ba969d4e5bbbc79b243
Joshua Staub <18756667+Carbonyte@users.noreply.github.com>
2026-03-24 12:15:15 -0400

    CarConfigPanel: incomplete work

--- CarConfigPanel.java
+++ CarConfigPanel.java
@@ -1,17 +1,62 @@
 //Lior Sapir
 
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.*;
 
-public class CarConfigPanel extends JPanel implements ActionListener {
-    
+public class CarConfigPanel extends JPanel implements ActionListener, DocumentListener {
+    private final JTextField carName;
+    private final JSlider speedSlider;
+    private final JButton removeButton;
+    private boolean readOnly = false;
+
     public CarConfigPanel() {
         setBorder(BorderFactory.createLineBorder(Color.BLUE, 2));
-        add(new JTextField("placeholder"));
-        JButton removeButton = new JButton("X");
+        setLayout(new GridBagLayout());
+
+        JLabel carNameLabel = new JLabel("Car Name:");
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 1;
+        c.anchor = GridBagConstraints.LINE_START;
+        add(carNameLabel, c);
+        carName = new JTextField(10);
+        carName.getDocument().addDocumentListener(this);
+        c = new GridBagConstraints();
+        c.gridx = 1;
+        c.gridy = 1;
+        c.anchor = GridBagConstraints.LINE_END;
+        add(carName, c);
+
+        JLabel speedLabel = new JLabel("Speed:");
+        c = new GridBagConstraints();
+        c.gridx = 0;
+        c.gridy = 2;
+        add(speedLabel, c);
+        speedSlider = new JSlider(JSlider.HORIZONTAL, 0, (int) Car.MAX_SPEED, 0);
+        c.gridx = 1;
+        add(speedSlider, c);
+
+        removeButton = new JButton("X");
+        c.gridx = 1;
+        c.gridy = 0;
+        removeButton.setActionCommand("remove");
         removeButton.addActionListener(this);
         add(removeButton);
+    }
+
+    public void setReadOnly(boolean readOnly) {
+        this.readOnly = readOnly;
+
+        carName.setEnabled(!readOnly);
+        speedSlider.setEnabled(!readOnly);
+        removeButton.setEnabled(!readOnly);
+    }
+
+    public boolean getReadOnly() {
+        return readOnly;
     }
 
     @Override
@@ -21,9 +66,32 @@ public class CarConfigPanel extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        Container parent = getParent(); 
-        parent.remove(this);
-        parent.revalidate();
-        parent.repaint();
+        if (e.getActionCommand().equals("remove")) {
+            Container parent = getParent();
+            parent.remove(this);
+            parent.revalidate();
+            parent.repaint();
+        }
+    }
+
+    public Car makeCar(int goalTrack, int totalTracks) {
+        Car car = new Car(carName.getText(), (goalTrack + 1) % totalTracks, goalTrack, totalTracks);
+        car.setSpeed(speedSlider.getValue());
+        return car;
+    }
+
+    @Override
+    public void insertUpdate(DocumentEvent e) {
+
+    }
+
+    @Override
+    public void removeUpdate(DocumentEvent e) {
+
+    }
+
+    @Override
+    public void changedUpdate(DocumentEvent e) {
+
     }
 }
