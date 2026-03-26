bbf181b18a7ca46473ce2f1b3ee7af734692112a
Joshua Staub <18756667+Carbonyte@users.noreply.github.com>
2026-03-26 12:42:05 -0400

    CarConfigPanel

--- CarConfigPanel.java
+++ CarConfigPanel.java
@@ -1,14 +1,16 @@
-//Lior Sapir
+//Lior Sapir, Joshua Staub
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
+import javax.swing.text.NumberFormatter;
 import java.awt.*;
 import java.awt.event.*;
+import java.text.NumberFormat;
 
-public class CarConfigPanel extends JPanel implements ActionListener, DocumentListener {
-    private final JTextField carName;
-    private final JSlider speedSlider;
+public class CarConfigPanel extends JPanel implements ActionListener {
+    private final JTextField nameField;
+    private final NumberField speedField;
     private final JButton removeButton;
     private boolean readOnly = false;
 
@@ -21,37 +23,45 @@ public class CarConfigPanel extends JPanel implements ActionListener, DocumentLi
         c.gridx = 0;
         c.gridy = 1;
         c.anchor = GridBagConstraints.LINE_START;
+        c.ipadx = 4;
         add(carNameLabel, c);
-        carName = new JTextField(10);
-        carName.getDocument().addDocumentListener(this);
+        nameField = new JTextField(10);
         c = new GridBagConstraints();
         c.gridx = 1;
         c.gridy = 1;
         c.anchor = GridBagConstraints.LINE_END;
-        add(carName, c);
+        add(nameField, c);
 
         JLabel speedLabel = new JLabel("Speed:");
         c = new GridBagConstraints();
         c.gridx = 0;
         c.gridy = 2;
+        c.anchor = GridBagConstraints.LINE_START;
         add(speedLabel, c);
-        speedSlider = new JSlider(JSlider.HORIZONTAL, 0, (int) Car.MAX_SPEED, 0);
+
+        int numDigits = (int) Math.ceil(Math.log10(Car.MAX_SPEED));
+        speedField = new NumberField(0, 0, (int) Car.MAX_SPEED);
+        speedField.setColumns(numDigits);
+        c = new GridBagConstraints();
         c.gridx = 1;
-        add(speedSlider, c);
+        c.gridy = 2;
+        c.anchor = GridBagConstraints.LINE_END;
+        add(speedField, c);
 
         removeButton = new JButton("X");
+        c = new GridBagConstraints();
         c.gridx = 1;
         c.gridy = 0;
+        c.anchor = GridBagConstraints.NORTHEAST;
         removeButton.setActionCommand("remove");
         removeButton.addActionListener(this);
-        add(removeButton);
+        add(removeButton, c);
     }
 
     public void setReadOnly(boolean readOnly) {
         this.readOnly = readOnly;
-
-        carName.setEnabled(!readOnly);
-        speedSlider.setEnabled(!readOnly);
+        nameField.setEnabled(!readOnly);
+        speedField.setEnabled(!readOnly);
         removeButton.setEnabled(!readOnly);
     }
 
@@ -75,23 +85,9 @@ public class CarConfigPanel extends JPanel implements ActionListener, DocumentLi
     }
 
     public Car makeCar(int goalTrack, int totalTracks) {
-        Car car = new Car(carName.getText(), (goalTrack + 1) % totalTracks, goalTrack, totalTracks);
-        car.setSpeed(speedSlider.getValue());
+        Car car = new Car(nameField.getText(), (goalTrack + 1) % totalTracks, goalTrack, totalTracks);
+
+        car.setSpeed(speedField.getCurValue());
         return car;
-    }
-
-    @Override
-    public void insertUpdate(DocumentEvent e) {
-
-    }
-
-    @Override
-    public void removeUpdate(DocumentEvent e) {
-
-    }
-
-    @Override
-    public void changedUpdate(DocumentEvent e) {
-
     }
 }
